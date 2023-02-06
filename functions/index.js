6da3b78e940f3f6977d2a8214c6bc3f499075757
erolkaftanoglu <erolkaftanoglu@gmail.com>
2023-02-07 02:49:00 +0300

    onDemandCreated function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
 const functions = require("firebase-functions");
+import fetch, {
+    Blob,
+    blobFrom,
+    blobFromSync,
+    File,
+    fileFrom,
+    fileFromSync,
+} from 'node-fetch'
+
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
@@ -25,3 +34,12 @@ exports.registerUser = functions.auth.user().onCreate(async (user) => {
         updatedAt: FieldValue.serverTimestamp()
     });
 });
+
+exports.onDemandCreate = functions.firestore
+    .document('demands/{docId}')
+    .onCreate(async (snap, context) => {
+        const demand = snap.data();
+        const response = await fetch(url, { method: 'POST', body: demand })
+        const data = await response.json()
+        console.log(data);
+    });
