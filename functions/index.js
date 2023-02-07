c0ed24be7347039db970aaa6e328502126aa46a2
Ogulcan Celik <ogulcancelik@gmail.com>
2023-02-07 19:39:15 +0300

    remove unused functions

--- functions/index.js
+++ functions/index.js
@@ -20,30 +20,6 @@ const configuration = {
 
 const limiter = FirebaseFunctionsRateLimiter.withFirestoreBackend(configuration, db)
 
-// helper function to calcualte distance between two points
-// https://stackoverflow.com/a/28673693/7951826
-//This function takes in latitude and longitude of two locations
-// and returns the distance between them as the crow flies (in meters)
-function calcCrow(coords1, coords2) {
-    // var R = 6.371; // km
-    var R = 6371000;
-    var dLat = toRad(coords2.lat - coords1.lat);
-    var dLon = toRad(coords2.lng - coords1.lng);
-    var lat1 = toRad(coords1.lat);
-    var lat2 = toRad(coords2.lat);
-
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    return d;
-}
-
-// Converts numeric degrees to radians
-function toRad(Value) {
-    return Value * Math.PI / 180;
-}
-
 exports.getDemands = functions.https.onRequest((req, res) => {
     cors(req, res, async () => {
         if (req.method !== 'POST') {
