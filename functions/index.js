86485dcb780a9b70151cded0c368515b1c004532
erolkaftanoglu <erolkaftanoglu@gmail.com>
2023-02-07 12:11:55 +0300

    Added get demands

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,41 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const fetch = require("node-fetch");
 
 const { initializeApp } = require('firebase-admin/app');
 const { getFirestore, FieldValue } = require('firebase-admin/firestore');
+const { FirebaseFunctionsRateLimiter } = require("firebase-functions-rate-limiter");
 
 initializeApp();
 
 const db = getFirestore();
+
+const configuration = {
+    name: "demands", // a collection with this name will be created
+    periodSeconds: 60, // the length of test period in seconds
+    maxCalls: 20,// number of maximum allowed calls in the period
+    debug: true // boolean (default false)
+};
+
+const limiter = FirebaseFunctionsRateLimiter.withFirestoreBackend(configuration, db)
+
+exports.getDemands = functions.https.onRequest(async (req, res) => {
+    if (req.method !== 'GET') {
+        res.status(405).send('Method Not Allowed');
+        return;
+    }
+    const quotaExceeded = await limiter.isQuotaAlreadyExceeded();
+    if (quotaExceeded) {
+        res.status(429).send('Too Many Requests');
+        return;
+    }
+    const demandsCollection = db.collection('demands');
+    const demands = await demandsCollection.get();
+    const demandsData = demands.docs.map(doc => doc.data());
+    res.send({
+        demands: demandsData
+    });
+});
 
 exports.registerUser = functions.auth.user().onCreate(async (user) => {
     const usersCollection = db.collection('users');
@@ -28,6 +57,7 @@ exports.registerUser = functions.auth.user().onCreate(async (user) => {
     });
 });
 
+
 exports.onUserUpdate = functions.firestore
     .document('users/{userId}')
     .onUpdate(async (change, context) => {
@@ -41,6 +71,7 @@ exports.onUserUpdate = functions.firestore
             await admin.auth().updateUser(userId, { disabled: isSuspended });
         }
     });
+
 
 exports.onDemandCreate = functions.firestore
     .document('demands/{docId}')
